3b394da119f3cd294848bd0ef3545b664cf60beb
Rafael <rafaelferreira.0096@gmail.com>
2022-02-17 14:59:25 -0300

    Implementação do sistema de rotas e criação do login de usuário

--- src/components/estaticos/footer/Footer.tsx
+++ src/components/estaticos/footer/Footer.tsx
@@ -3,30 +3,31 @@ import { Typography, Grid, Box } from "@material-ui/core";
 import InstagramIcon from '@material-ui/icons/Instagram';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import './Footer.css';
 
 function Footer() {
     return (
         <>
             <Grid container direction="row" justifyContent="center" alignItems="center">
                 <Grid alignItems="center" item xs={12}>
-                    <Box style={{ backgroundColor: "grey", height: "120px" }}>
+                    <Box className="box1">
                         <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-                            <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}></Typography>
+                            <Typography className="text" variant="h5" align="center" gutterBottom>Redes sociais</Typography>
                         </Box>
-                        <Box className="rodape" display="flex" alignItems="center" justifyContent="center">
+                        <Box className="footer-social" display="flex" alignItems="center" justifyContent="center">
                             <a href="https://www.instagram.com/rafaelsotnas/">
-                                <InstagramIcon style={{ fontSize: 60, color: "white" }} />
+                                <InstagramIcon className="footer-social" />
                             </a>
                             <a href="https://github.com/rafaelsotnas">
-                                <GitHubIcon style={{ fontSize: 60, color: "white" }} />
+                                <GitHubIcon className="footer-social" />
                             </a>
                             <a href="https://www.linkedin.com/in/rafael-ferreira-7b6600205/">
-                                <LinkedInIcon style={{ fontSize: 60, color: "white" }} />
+                                <LinkedInIcon className="footer-social" />
                             </a>
                         </Box>
-                        <Box style={{ backgroundColor: "darkgrey" }}>
+                        <Box className="box2">
                             <Box paddingTop={1}>
-                                <Typography className="rodape2" variant="subtitle2" align="center" gutterBottom style={{ color: "white" }} >© 2022 Copyright</Typography>
+                                <Typography className="footer-copy" variant="subtitle2" align="center" gutterBottom>© 2022 Copyright</Typography>
                             </Box>
                         </Box>
                     </Box>
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -8,14 +8,14 @@ function Home() {
             <Grid className="back" container direction="row" justifyContent="center" alignItems="center">
                 <Grid alignItems="center" item xs={6}>
                     <Box paddingX={20}>
-                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>Expresse-se!</Typography>
+                        <Typography className="titulo" variant="h5" gutterBottom color="textPrimary" component="h5" align="center">Expresse-se!</Typography>
                     </Box>
                     <Box display="flex" justifyContent="center">
                         <Box marginRight={1}>
                         </Box>
-                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#9e9e9e", color: "white" }}>Ver postagens</Button>
+                        <Button className="button-color" variant="outlined">Ver postagens</Button>
                     </Box>
-                    <Grid xs={12}></Grid>
+                    <Grid className="posts" xs={12}></Grid>
                 </Grid>
             </Grid>
         </>
